d142572c8a53b0a2ada1d02b35d0193b505f6e51
Cam Dang <camdang@Cam-ne.local>
2025-02-02 22:25:11 +0700

    add automation fw

---
+++ Project_Test/src/main/Java/automation/common/CommonBase.java
@@ -0,0 +1,7 @@
+package automation.common;
+
+public class CommonBase {
+    public static void main(String[] args) {
+        // TODO: Auto-generated method stub
+    }
+}
---
+++ Project_Test/src/main/Java/automation/constant/CT_PageURL.java
@@ -0,0 +1,7 @@
+package automation.constant;
+
+public class CT_PageURL {
+    public static void main(String[] args) {
+        // TODO: Auto-generated method stub
+    }
+}
